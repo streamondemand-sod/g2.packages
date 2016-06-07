6af15c06215d8bddcc00cc606efa9d0b00ab6a26
J0rdy Z <jrdy.z65@gmail.com>
2016-06-07 15:48:06 +0000

    Added kind attribute

--- dbs/__init__.py
+++ dbs/__init__.py
@@ -4,7 +4,7 @@ kind = 'dbs'
 """The :kind: attribute specify the kind of package"""
 
 site = 'github://J0rdyz65/g2.packages/dbs'
-"""The :site: attribute is the pseudo-URL used to fetch the directory content in JSON format from GitHub."""
+"""The :site: attribute is the pseudo-URL used to fetch the directory content in JSON format from GitHub"""
 
 addons = []
-"""The :addons: attribute is a list of kodi addons names required by the package."""
+"""The :addons: attribute is a list of kodi addons names required by the package"""
