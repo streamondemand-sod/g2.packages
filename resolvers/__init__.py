bb5c3101e2017a23019bdb4c4c5362b83cfff843
J0rdy Z <jrdy.z65@gmail.com>
2016-06-07 17:01:10 +0000

    Added kind attribute

--- resolvers/__init__.py
+++ resolvers/__init__.py
@@ -6,5 +6,5 @@ kind = 'resolvers'
 site = 'github://J0rdyz65/g2.packages/resolvers'
 """The :site: attribute is the pseudo-URL used to fetch the directory content in JSON format from GitHub"""
 
-addons = None
+addons = []
 """The :addons: attribute is a list of kodi addons names required by the package"""
